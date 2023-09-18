516f15f34801f0946b7445db7fe7fb86dc0dc360
Jayne Valverde <jaynevalverde@gmail.com>
2023-09-18 13:01:50 -0500

    savedBooks.js/pages, worked on

--- client/src/pages/SavedBooks.js
+++ client/src/pages/SavedBooks.js
@@ -1,67 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import {
-  Container,
-  Card,
-  Button,
-  Row,
-  Col
-} from 'react-bootstrap';
-
-import { getMe, deleteBook } from '../utils/API';
+// React setup
+import React from 'react';
+import { Jumbotron, Container, CardColumns, Card, Button } from 'react-bootstrap';
+// import the 'auth' setup
 import Auth from '../utils/auth';
 import { removeBookId } from '../utils/localStorage';
+// need to refactor for GrapQL API
+import { useQuery, useMutation} from '@apollo/client';
+import { REMOVE_BOOK } from '../utils/mutations';
+import { GET_ME } from '../utils/queries';
 
 const SavedBooks = () => {
-  const [userData, setUserData] = useState({});
+  const { loading, data } = useQuery(GET_ME);
+  const [removeBook, { error }] = useMutation(REMOVE_BOOK);
 
-  // use this to determine if `useEffect()` hook needs to run again
-  const userDataLength = Object.keys(userData).length;
+  const userData = data?.me || {};
 
-  useEffect(() => {
-    const getUserData = async () => {
-      try {
-        const token = Auth.loggedIn() ? Auth.getToken() : null;
-
-        if (!token) {
-          return false;
-        }
-
-        const response = await getMe(token);
-
-        if (!response.ok) {
-          throw new Error('something went wrong!');
-        }
-
-        const user = await response.json();
-        setUserData(user);
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getUserData();
-  }, [userDataLength]);
-
-  // create function that accepts the book's mongo _id value as param and deletes the book from the database
+  // function to delete book from database
   const handleDeleteBook = async (bookId) => {
     const token = Auth.loggedIn() ? Auth.getToken() : null;
 
-    if (!token) {
+    if(!token) {
       return false;
     }
 
+    // new code
     try {
-      const response = await deleteBook(bookId, token);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const updatedUser = await response.json();
-      setUserData(updatedUser);
-      // upon success, remove book's id from localStorage
-      removeBookId(bookId);
+      const response = await removeBook({ variables: { bookId } });
+        console.log('Deleted record: ', response);
+        if (error) {
+          console.log(error);
+        }
+        // also remove from localstorage
+        removeBookId(bookId);
     } catch (err) {
+      // display caught errors here
       console.error(err);
     }
   };
@@ -73,21 +45,20 @@ const SavedBooks = () => {
 
   return (
     <>
-      <div fluid className='text-light bg-dark p-5'>
+      <Jumbotron fluid className='text-light bg-dark p-5'>
         <Container>
           <h1>Viewing saved books!</h1>
         </Container>
-      </div>
+      </Jumbotron>
       <Container>
-        <h2 className='pt-5'>
+        <h2>
           {userData.savedBooks.length
             ? `Viewing ${userData.savedBooks.length} saved ${userData.savedBooks.length === 1 ? 'book' : 'books'}:`
             : 'You have no saved books!'}
         </h2>
-        <Row>
+        <CardColumns>
           {userData.savedBooks.map((book) => {
             return (
-              <Col md="4">
                 <Card key={book.bookId} border='dark'>
                   {book.image ? <Card.Img src={book.image} alt={`The cover for ${book.title}`} variant='top' /> : null}
                   <Card.Body>
@@ -99,10 +70,9 @@ const SavedBooks = () => {
                     </Button>
                   </Card.Body>
                 </Card>
-              </Col>
             );
           })}
-        </Row>
+        </CardColumns>
       </Container>
     </>
   );
