7b575958e3a50cc9e3b43064180a57097f461e8a
Jayne Valverde <jaynevalverde@gmail.com>
2023-09-19 12:00:37 -0500

    savedbooks edit

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
+import Auth from '../utils/auth'
 import { createUser } from '../utils/API';
 import Auth from '../utils/auth';
 
