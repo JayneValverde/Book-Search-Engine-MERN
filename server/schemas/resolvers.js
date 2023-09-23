a30767dfb2950a122d8cd9c4af8c78a29d1a4289
Jayne Valverde <jaynevalverde@gmail.com>
2023-09-23 09:15:40 -0500

    recent changes

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,19 +4,23 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = { 
     Query: {
+
         me: async (parent, args, context) => {
             // checks if users exist
             if (context.user) {
-                const userdata = await User.findOne({ _id: context.user._id }).select(
-                    '__v - password'
-                    );
-                    return userdata;
+                const userData = await User.findOne({ _id: context.user._id })
+                    .select('__v - password')
+                    .populate('savedBooks');
+
+                    return userData;
                 }
                 throw new AuthenticationError("Must be logged in!")
         },
     },
+
     Mutation: {
-        login: async (parent, { email, password } ) => {
+
+        login: async (parent, { email, password }) => {
             const user = await User.findOne({ email });
             // check if user exists with email and credentials
             if (!user) {
@@ -31,12 +35,14 @@ const resolvers = {
             const token = signToken(user);
             return { token, user };
         },
+
         addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
 
             return { token, user };
         },
+
         saveBook: async (parent, { newBook }, context) => {
             if (context.user) {
                 const updatedUser = await User.findOneAndUpdate(
@@ -59,7 +65,7 @@ const resolvers = {
             }
             throw new AuthenticationError('Login required');
         },
-    }
+    },
 };
 
 module.exports = resolvers;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,9 +5,15 @@ const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
 module.exports = {
+  signToken: function({ username, email, _id }) {
+    const payload = { username, email, _id };
+
+    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+  },
+
   authMiddleware : function ({ req }) {
     // allows token to be sent via  req.query or headers
-    let token = req.body.token || req.query.token || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization ;
 
     // ["Bearer", "<tokenvalue>"]
     if (req.headers.authorization) {
@@ -28,10 +34,6 @@ module.exports = {
 
     return req;
   },
-  signToken: function({ username, email, _id }) {
-    const payload = { username, email, _id };
 
-    return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
-  }
 };
 
